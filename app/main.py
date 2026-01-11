dd8790d5daec6d95db4f0b3d16f51f141715bb73
MARTIN MA <124866191+MartinYu0510@users.noreply.github.com>
2026-01-11 20:16:01 +0800

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,276 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse, StreamingResponse
+import uvicorn
+import numpy as np
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.metrics import silhouette_score
+import sys
+from ultralytics import YOLO
+import os
+from pathlib import Path
+
+# db clustering to detect lines
+def detect_lines_dbscan(y_coords, eps_auto=True, safety_factor=5):
+    y_array = np.array(y_coords).reshape(-1, 1)
+    
+    if eps_auto:
+        from sklearn.neighbors import NearestNeighbors
+        
+        neighbors = NearestNeighbors(n_neighbors=2)
+        neighbors_fit = neighbors.fit(y_array)
+        distances, indices = neighbors_fit.kneighbors(y_array)
+        
+        distances = np.sort(distances[:, 1])
+        
+        # we want epsilon to be max nearest within cluster neighbor distance
+        # set to be 95% * safety factor to prevent extreme cases with single point cluster
+        eps = np.percentile(distances, 95) * safety_factor
+    else:
+        eps = 10  # approx char height
+    
+    dbscan = DBSCAN(eps=eps, min_samples=1)
+    labels = dbscan.fit_predict(y_array)
+    
+    return labels
+
+classNameToBraille = {
+    'a': '⠁', 'b': '⠃', 'c': '⠉', 'd': '⠙', 'e': '⠑',
+    'f': '⠋', 'g': '⠛', 'h': '⠓', 'i': '⠊', 'j': '⠚',
+    'k': '⠅', 'l': '⠇', 'm': '⠍', 'n': '⠝', 'o': '⠕',
+    'p': '⠏', 'q': '⠟', 'r': '⠗', 's': '⠎', 't': '⠞',
+    'u': '⠥', 'v': '⠧', 'w': '⠺', 'x': '⠭', 'y': '⠽', 'z': '⠵',
+    'capital': '⠠',
+    'number': '⠼',
+    'dot_4': '',
+    
+    # # Punctuation
+    # '.': '⠲',    # period
+    # ',': '⠂',    # comma
+    # '?': '⠦',    # question mark
+    # '!': '⠖',    # exclamation mark
+    # ';': '⠆',    # semicolon
+    # ':': '⠒',    # colon
+    # "'": '⠄',    # apostrophe
+    # '"': '⠐⠂',  # quotation mark
+    # '-': '⠤',    # hyphen
+    # '(': '⠶',    # opening parenthesis
+    # ')': '⠶',    # closing parenthesis (same as opening in Braille)
+    # '/': '⠌',    # slash
+    # '[': '⠦',    # opening bracket (same as ? in some systems)
+    # ']': '⠴',    # closing bracket
+    # '{': '⠠⠦',  # opening brace
+    # '}': '⠠⠴',  # closing brace
+    # '<': '⠪',    # less than
+    # '>': '⠕',    # greater than (same as o in some systems)
+}
+
+numberDict = {
+    '0': '⠚', '1': '⠁', '2': '⠃', '3': '⠉', '4': '⠙',
+    '5': '⠑', '6': '⠋', '7': '⠛', '8': '⠓', '9': '⠊',
+}
+
+app = FastAPI(title="Braille Recognition API")
+
+
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Braille Recognition API", "status": "running", "Current Directory": Path(__file__).parent}
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    model = None
+    current_dir = Path(__file__).parent
+    model_path = None
+    if((current_dir / "model").exists()):
+        model_path = current_dir / "model" / "currentModel.pt"
+    else:
+        model_path = Path('/app/model/currentModel.pt')
+    try:
+        model = YOLO(model_path)
+        # if not file.content_type.startswith('image/'):
+        #     raise HTTPException(status_code=400, detail="File must be an image")
+    
+        if model is None: 
+            raise HTTPException(status_code=500, detail="Model not loaded")
+        
+        try: 
+            contents = await file.read()
+            
+            import PIL.Image
+            import io as image_io
+            
+            image = PIL.Image.open(image_io.BytesIO(contents))
+            
+            results = model(image)
+            
+            # process results        
+            cords = []
+            confidences = []
+            
+            results[0].show()
+            
+            temp = [(item["name"], item["box"]["y1"], item["box"]["x1"]) for item in results[0].summary()]
+            temp = sorted(temp, key=lambda x: x[1])
+            for item in temp:
+                print(item)
+                
+            print()
+            
+            if results[0].boxes is not None and len(results[0].boxes) > 0: 
+                boxes = results[0].boxes
+                for box in boxes: 
+                    cls_idx = int(box.cls[0])
+                    class_name = model.names[cls_idx]
+                    conf = float(box.conf[0])
+                    
+                    x1, y1, x2, y2 = box.xyxy[0].tolist()
+                    midpoint = ((x1+x2)/2, (y1+y2)/2)
+                    
+                    cords.append((class_name, midpoint))
+                    confidences.append(conf)
+                    
+
+            if not cords:
+                return JSONResponse({
+                    "braille": "",
+                    "text": "",
+                    "message": "No braille characters detected",
+                    "confidence": 0
+                })
+                    
+            # group together similar y as same line
+            y_cords = [x[1][1] for x in cords]
+            line_labels = detect_lines_dbscan(y_cords)
+        
+            line_groups = {}
+            for i, label in enumerate(line_labels):
+                if label not in line_groups:
+                    line_groups[label] = []
+                line_groups[label].append(cords[i])
+                
+            sorted_lines = []
+            for label in sorted(line_groups.keys(),  key=lambda l: np.mean([c[1][1] for c in line_groups[l]])):
+                line_chars = sorted(line_groups[label], key=lambda x: x[1][0])
+                sorted_lines.append(line_chars)
+                
+            
+            for line in sorted_lines:
+                for char in line:
+                    print(f"{char[0]}", end=" ")
+                print()
+                
+            print()
+            
+            horizontal_distance_from_front = []
+            for line in sorted_lines: 
+                x_dist_within_line = [-1]
+                for i in range(1, len(line)): 
+                    x_dist_within_line.append(line[i][1][0] - line[i-1][1][0])
+                horizontal_distance_from_front.append(x_dist_within_line)
+            
+            for line in horizontal_distance_from_front:
+                for char in line:
+                    print(f"{char:.2f}", end=" ")
+                print()
+                
+            same_line_horizontal_dist = [dist for sublist in horizontal_distance_from_front for dist in sublist if dist > 0]
+            
+
+            # try clustering to two clusters
+            if len(same_line_horizontal_dist) >= 2: 
+                X = np.array(same_line_horizontal_dist).reshape(-1, 1)
+                kmeans = KMeans(n_clusters=2, random_state=0, n_init=10).fit(X)
+                labels = kmeans.fit_predict(X)
+                centers = sorted(kmeans.cluster_centers_.flatten())
+                
+                # silhouette check: 
+                score = silhouette_score(X, labels)
+                
+                if score > 0.5: 
+                    space_threshold = np.mean(centers)
+                else: 
+                    space_threshold = sys.maxsize            
+            else: 
+                space_threshold = sys.maxsize
+                
+            print(space_threshold)
+            
+            processed_braille = ""
+            processed_text = ""
+            capitalFlag = False
+            numberFlag = False
+            for i, line in enumerate(sorted_lines): 
+                for j, char in enumerate(line): 
+                    if (horizontal_distance_from_front[i][j] == -1 and i != 0):
+                        # newline? 
+                        processed_braille += " "
+                        processed_text += " "
+                    elif (horizontal_distance_from_front[i][j] > space_threshold):
+                        processed_braille += " "
+                        processed_text += " "
+                        
+                    processed_braille += classNameToBraille[char[0]]
+                    if char[0] == "capital": 
+                        capitalFlag = True
+                    elif char[0] == "number": 
+                        numberFlag = True
+                    else: 
+                        if capitalFlag: 
+                            processed_text += char[0].upper()
+                            capitalFlag = False
+                        elif numberFlag: 
+                            processed_text += numberDict[char[0]]
+                            numberFlag = False
+                        else: 
+                            processed_text += char[0]
+            
+            avg_confidence = np.mean(confidences) if confidences else 0
+            
+            return JSONResponse({
+                "braille": processed_braille,
+                "text": processed_text,
+                "confidence": avg_confidence
+            })
+            
+        except Exception as e: 
+            raise HTTPException(status_code=500, detail=f"Processing error: {str(e)}")
+    except Exception as e:
+        print(f"Error loading model: {e} in directory {os.getcwd()}")
+        model = None
+    
+
+# @app.post("/predict/visualize/")
+# async def predict_visualize(file: UploadFile = File(...)):
+#     if not file.content_type.startswith('image/'):
+#         raise HTTPException(status_code=400, detail="File must be an image")
+    
+#     contents = await file.read()
+#     annotated_image = model.predict_and_draw(contents)
+    
+#     return StreamingResponse(
+#         io.BytesIO(annotated_image),
+#         media_type="image/jpeg",
+#         headers={"Content-Disposition": f"attachment; filename=result_{file.filename}"}
+#     )
+
+@app.get("/classes/")
+async def get_classes():
+    model = None
+    current_dir = Path(__file__).parent
+    model_path = None
+    if((current_dir / "model").exists()):
+        model_path = current_dir / "model" / "currentModel.pt"
+    else:
+        model_path = Path('/app/model/currentModel.pt')
+    try:
+        model = YOLO(model_path)
+        return {"classes": model.names}
+    except Exception as e:
+        print(f"Error loading model: {e} in directory {os.getcwd()}")
+        model = None
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
